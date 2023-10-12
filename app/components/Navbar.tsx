f2e9adf3d5d0ff356c79d37402f7a19b2c658109
ahmed nur <ahmedlearn01@gmail.com>
2023-10-12 08:48:13 +0300

    fonts, styles and images

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,8 +1,17 @@
 import Link from 'next/link'
+import logo from './dojo-logo.png'
+import Image from 'next/image'
 
 export default function Navbar() {
   return (
     <nav>
+    <Image 
+    src={logo}
+    alt='Cs Dojo'
+    placeholder='blur'
+    quality={100}
+    width={70}
+    />
     <h1>Nextjs 13</h1>
     <Link href={'/'}>Dashboard</Link>
     <Link href={'/tickets'}>Tickets</Link>
