4093c7eac49a7287f7f142636b2935531945c952
ahmed nur <ahmedlearn01@gmail.com>
2023-10-14 09:48:26 +0300

    suspense & loading

---
+++ app/loading.tsx
@@ -0,0 +1,8 @@
+export default function Loading() {
+  return (
+    <main className="text-center">
+        <h3 className="text-primary">Loading...</h3>
+        <p>Not too long...</p>
+    </main>
+  )
+}
